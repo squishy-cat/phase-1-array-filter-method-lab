df8a3db1affc67d08f50c5a73eda60208d3f3ac2
Eve Kozina <eve.kozina@gmail.com>
2021-11-03 16:21:48 -0400

    commit tree a395fdb95f2cf24777137bfba18731db768fabd4

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array,query) {
+    return array.filter(function(drivers) {
+        return drivers.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+}
+
+function fuzzyMatch(array,query) {
+    return array.filter(function(driverLetters) {
+        return driverLetters.indexOf(query) !==-1 
+        && 
+        driverLetters.slice(2, driverLetters.length).indexOf(query) === -1
+    })
+}
+
+function matchName(drivers,enteredName) {
+    return drivers.filter(function(driver){
+        return driver.name === enteredName
+    })
+}
